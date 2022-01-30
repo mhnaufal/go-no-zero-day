55bbbb78fe3c6d4a401a2e09a69ba90614fb06e5
mhnaufal <aal.pirates16@gmail.com>
2022-01-31 04:52:49 +0700

    day_8

---
+++ day_8/closure.go
@@ -0,0 +1,36 @@
+package day_8
+
+import "fmt"
+
+func Closure() {
+	index := 0
+
+	incrementIndex := func() int {
+		index *= 2 //can access the outer variables
+		return index
+	}
+
+	fmt.Printf("Index value = %v\n", incrementIndex())
+	fmt.Printf("Index value = %v\n", incrementIndex())
+
+	fmt.Println("-----------------------------------------------")
+
+	next := sequence()
+
+	fmt.Printf("Sequence = %v\n", next())
+	fmt.Printf("Sequence = %v\n", next())
+	fmt.Printf("Sequence = %v\n", next())
+
+	newNext := sequence() //restart from 1
+	fmt.Printf("New Sequence = %v\n", newNext())
+
+}
+
+// func functionName returnFunc returnFuncType {}
+func sequence() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
