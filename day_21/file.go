017f256fde01ce0588d5a96b01e752928d893d05
mhnaufal <aal.pirates16@gmail.com>
2022-02-21 05:27:32 +0700

    day 22

--- day_21/file.go
+++ day_21/file.go
@@ -8,12 +8,13 @@ import (
 var path = "C:/Users/Ryzen/Desktop/Projects4/Go/no-zero-day/day_21/file.txt"
 
 func File() {
+	// Creating a new file
 	var _, err = os.Stat(path)
 
 	if os.IsNotExist(err) {
 		var file, err = os.Create(path)
 
-		if err == nil {
+		if err != nil {
 			return
 		}
 
@@ -21,4 +22,28 @@ func File() {
 	}
 
 	fmt.Println("File => ", path)
+
+	/* Day 22 */
+	// Writing into file
+	var file, err2 = os.OpenFile(path, os.O_RDWR, 0644)
+
+	if err2 != nil {
+		return
+	}
+
+	defer file.Close()
+
+	_, err2 = file.WriteString("Hello all!\n") //write into the file
+
+	if err2 != nil {
+		return
+	}
+
+	err2 = file.Sync() //save the changes
+
+	if err2 != nil {
+		return
+	}
+
+	fmt.Println("File updated => ", file)
 }
