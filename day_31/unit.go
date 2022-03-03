193073ee1e3420931c4b41dcd6e09a905a1ca62d
mhnaufal <aal.pirates16@gmail.com>
2022-03-04 06:17:26 +0700

    day 31

---
+++ day_31/unit.go
@@ -0,0 +1,23 @@
+package day_31
+
+import "math"
+
+type Kubus struct {
+	Sisi float64
+}
+
+func (k Kubus) Volume() float64 {
+	return math.Pow(k.Sisi, 3)
+}
+
+func (k Kubus) Luas() float64 {
+	return math.Pow(k.Sisi, 2) * 6
+}
+
+func (k Kubus) Keliling() float64 {
+	return k.Sisi * 12
+}
+
+func Test() {
+
+}
