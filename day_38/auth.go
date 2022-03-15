9f2aa0aea055d7064737ed35bceb750c5e67372e
mhnaufal <aal.pirates16@gmail.com>
2022-03-16 05:59:12 +0700

    day 38 39

---
+++ day_38/auth.go
@@ -0,0 +1,51 @@
+package main
+
+import "net/http"
+import "fmt"
+import "encoding/json"
+
+func main() {
+	http.HandleFunc("/student", ActionStudent)
+
+	server := new(http.Server)
+	server.Addr = ":3000"
+
+	fmt.Println("server started at localhost:3000")
+	server.ListenAndServe()
+}
+
+func ActionStudent(w http.ResponseWriter, r *http.Request) {
+	if !Auth(w, r) {
+		return
+	}
+	if !AllowOnlyGET(w, r) {
+		return
+	}
+
+	if id := r.URL.Query().Get("id"); id != "" {
+		OutputJSON(w, SelectStudent(id))
+		return
+	}
+
+	OutputJSON(w, GetStudents())
+}
+
+func OutputJSON(w http.ResponseWriter, o interface{}) {
+	res, err := json.Marshal(o)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}
+
+func AllowOnlyGET(w http.ResponseWriter, r *http.Request) bool {
+    if r.Method != "GET" {
+        w.Write([]byte("Only GET is allowed"))
+        return false
+    }
+
+    return true
+}
---
+++ day_38/middleware.go
@@ -0,0 +1,22 @@
+package main
+
+import "net/http"
+
+const USERNAME = "joker"
+const PASSWORD = "riddle"
+
+func Auth(w http.ResponseWriter, r *http.Request) bool {
+    username, password, ok := r.BasicAuth()
+    if !ok {
+        w.Write([]byte(`Error`))
+        return false
+    }
+
+    isValid := (username == USERNAME) && (password == PASSWORD)
+    if !isValid {
+        w.Write([]byte(`Wrong username or password`))
+        return false
+    }
+
+    return true
+}
