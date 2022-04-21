f7b9f43bc4e119792af9b904322ffe6a7744531e
mhnaufal <aal.pirates16@gmail.com>
2022-04-22 05:53:50 +0700

    day 52

--- day_51/gin.go
+++ day_51/gin.go
@@ -1,14 +1,43 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Private user data
+var users = gin.H{
+	"Joker":      gin.H{"email": "joker@asylum.com", "phone": "08123"},
+	"The Riddle": gin.H{"email": "riddle@asylum.com", "phone": "08321"},
+}
 
 func main() {
 	r := gin.Default()
+
+	// BasicAuth => return basic HTTP middleare
+	// => take a gin.Accounts, where key is user, value is the password
+	// => gin.Accounts form a map[string]string
+	authorized := r.Group("/admin", gin.BasicAuth(gin.Accounts{
+		"Joker":      "mysecretpassword",
+		"The Riddle": "pass123",
+	}))
+
+	// MustGet => check if the given key is exists
+	authorized.GET("/secret", func(c *gin.Context) {
+		user := c.MustGet(gin.AuthUserKey).(string)
+		if secret, ok := users[user]; ok {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": secret})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET"})
+		}
+	})
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"status": "SUCCESS",
 		})
 	})
 
-	r.Run("127.0.0.1:3000")
+	r.Run("127.0.0.1:5000")
 }
