de8ec600722547022be267260f3e5431321f9962
mhnaufal <aal.pirates16@gmail.com>
2022-05-26 13:51:52 +0700

    update main

--- CONTINUE/main.go
+++ CONTINUE/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	c_two "no-zero-day/CONTINUE/chapter2"
 	c_three "no-zero-day/CONTINUE/chapter3"
-	// c_four "no-zero-day/CONTINUE/chapter4"
-	// c_five "no-zero-day/CONTINUE/chapter5"
+	c_four "no-zero-day/CONTINUE/chapter4"
+	c_five "no-zero-day/CONTINUE/chapter5"
+	c_six "no-zero-day/CONTINUE/chapter6"
 )
 
 func main() {
 	c_two.Type()
 	c_three.Composite()
-	// c_four.Control()
-	// c_five.Funtion()
+	c_four.Control()
+	c_five.Funtion()
+	c_six.Pointer()
 }
