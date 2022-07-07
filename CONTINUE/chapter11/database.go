45cda96d790fec9e1f3be9463a3d194509851b9f
mhnaufal <aal.pirates16@gmail.com>
2022-07-08 04:12:14 +0700

    query arguments

--- CONTINUE/chapter11/database.go
+++ CONTINUE/chapter11/database.go
@@ -6,7 +6,7 @@ import (
 )
 
 func GetConnection() *sql.DB {
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/mail_template_go2")
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/mail_template_go2?parseTime=true")
 	if err != nil {
 		panic(err)
 	}
--- CONTINUE/chapter11/sql_test.go
+++ CONTINUE/chapter11/sql_test.go
@@ -45,3 +45,31 @@ func TestSelectSql(t *testing.T) {
 		fmt.Printf("ID: %v\nNama: %v\n", id, nama)
 	}
 }
+
+func TestQueryWithArguments(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	nama := "utama"
+	qquery := "SELECT id, nama FROM surat WHERE nama = ? LIMIT 1"
+
+	row, err := db.QueryContext(ctx, qquery, nama)
+
+	if err != nil {
+		panic(err)
+	}
+	defer row.Close()
+
+	for row.Next() {
+		var id, nama string
+
+		err = row.Scan(&id, &nama)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("ID: %v\nNama: %v\n", id, nama)
+	}
+}
