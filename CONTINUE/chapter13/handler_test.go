334a3ba4273822a2c712058a16e397094105a629
mhnaufal <aal.pirates16@gmail.com>
2022-07-25 05:13:12 +0700

    request & mux

--- CONTINUE/chapter13/handler_test.go
+++ CONTINUE/chapter13/handler_test.go
@@ -23,3 +23,35 @@ func TestHandler(t *testing.T) {
 		log.Fatalln("Server failed to start")
 	}
 }
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hello mom")
+	})
+
+	mux.HandleFunc("/healthcheck", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Healthy")
+	})
+
+	server := http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalln("Server failed to start")
+	}
+}
+
+func TestRequest(t *testing.T) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, r.Body)
+		fmt.Fprint(w, r.Method)
+		fmt.Fprint(w, r.RequestURI)
+	})
+}
